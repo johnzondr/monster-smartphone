9456f18f2b626407738bd68831e07480d6128257
John Zhao <john@zondr.com>
2015-09-23 06:52:11 +0000

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scalemail::Application.config.secret_key_base = 'e22875c8b9602ba61c8ec78d40f275a13b84de393540cb974a8f91f71c8f1b8071a0da1ae952073d016c2fbb974057b0212731d81c9f5a748575a83d7a157145'
